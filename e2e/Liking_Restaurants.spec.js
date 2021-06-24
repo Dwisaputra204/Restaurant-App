e24e3115228f501a2a3ac176ee6947a4f8c6308b
Dwisaputra204 <dwisaputra204@gmail.com>
2021-06-24 11:01:46 +0700

    e2e testing

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,5 +1,4 @@
-const { pause } = require('codeceptjs');
-
+const assert = require('assert');
 /* eslint-disable no-undef */
 Feature('Liking Movies');
 
@@ -12,18 +11,24 @@ Scenario('showing empty liked movies', ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '#restaurant');
 });
 
-Scenario('liking one restaurant', ({ I }) => {
+Scenario('liking one restaurant', async ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '#restaurant');
 
   I.amOnPage('/');
 
   I.seeElement('.content-head a');
-  I.click(locate('.content-head a').first());
+
+  const firstRestaurant = locate('.content-head a').first();
+  const firstRestautantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('figure');
+  const likedRestaurantTitle = await I.grabTextFrom('.content-head a');
+
+  assert.strictEqual(firstRestautantTitle, likedRestaurantTitle);
   // … kita akan mengisi uji coba berikutnya …
 });
